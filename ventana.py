ef7b97acddb32160aee6e006e21c676400157adb
Alberto Cano Delgado <canoalboroteo@gmail.com>
2021-07-11 21:43:07 +0200

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -2,4 +2,10 @@ import tkinter
 
 ventana = tkinter.Tk()
 
+#Size of window
+ventana.geometry("500x500")
+
+etiqueta = tkinter.Label(ventana, text = "Aaaaaaa", bg = "blue")
+etiqueta.pack(side = tkinter.BOTTOM, fill=tkinter.X)
+
 ventana.mainloop()
